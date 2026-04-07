a8f2de8b7c702e86bc32e108cb1e67a37e4068f2
MrNaniii <krestovskijaleksandr379@gmail.com>
2026-04-07 17:49:09 +0200

    celan old files

--- Logic/IMoveRule.cs
+++
@@ -1,9 +0,0 @@
-using ChessLib.Core;
-using ChessLib.Pieces;
-
-namespace ChessLib.Logic;
-
-public interface IMoveRule
-{
-    public List<Position> GetMoves(Board board, Position pos, Piece piece, GameState state);
-}
